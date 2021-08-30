cbe1f5cd88123c493ab5088d294256649bf3d2a5
Katrín María <katr240j@stud.kea.dk>
2021-08-30 15:58:13 +0200

    capitalization3

--- nameparts.js
+++ nameparts.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const fullName = "Peter Heronimous Lind";
+
+
+//med magic nummers
+/* const firtsName = fullName.substring(0,5);
+const middleName = fullName.substring(6,16);
+const lastName = fullName.substring(17,21);
+
+console.log(firtsName);
+console.log(middleName);
+console.log(lastName); */
+
+const firstName = fullName.substring(0, fullName.indexOf(' '));
+console.log(`the first name of ${fullName} is _${firstName}_`);
+
+const middleName = fullName.substring(fullName.indexOf(' ')+1,fullName.lastIndexOf(' '));
+console.log(`the second name of ${fullName} is _${middleName}_`);
+
+const lastName = fullName.substring(fullName.lastIndexOf(' '));
+console.log(`the second name of ${fullName} is _${lastName}_`);
+
