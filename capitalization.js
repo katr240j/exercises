da9ce6f9e6dbb831f5b41f7d3aad0c17a43bfb5a
Katrín María <katr240j@stud.kea.dk>
2021-08-30 16:16:57 +0200

    capitalization opgave

---
+++ capitalization.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let navn = "katrin"; 
+ 
+
+let nyNavn = navn[0].toUpperCase() + navn.substring(1); 
+
+console.log(nyNavn); 
+
--- capitalization3.js
+++ capitalization3.js
@@ -1,1 +1,10 @@
 "use strict";
+
+let navn = "peteration"; 
+
+ // með því að setja (3) en ekki (3,5) þá getur nafnið alltaf verið lengra og þarf þvi ekki að stoppa á 5 bókstaf
+ 
+
+let nyNavn = navn.substring(0,2) + navn[2].toUpperCase() + navn.substring(3); 
+
+console.log(nyNavn); 
