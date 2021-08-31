0e9ee3ef8a454d1c51327f6cf9beb2b198869df6
Katrín María <katr240j@stud.kea.dk>
2021-08-31 13:14:33 +0200

    initLoop

---
+++ initLoop.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let maxNumberOfIterations;
+let iterator;
+
+init()
+
+function init() {
+    maxNumberOfIterations = 11;
+    iterator = 0;
+
+    loop();
+}
+
+function loop() {
+    console.log('loop', iterator);
+    iterator++;
+
+    if(iterator < maxNumberOfIterations)    {
+setTimeout(loop,500);
+    }
+}
