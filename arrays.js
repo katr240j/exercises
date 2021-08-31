7590b449b0c7963de62af36e75cdcf5e74a5ef41
Katrín María <katr240j@stud.kea.dk>
2021-08-31 14:56:08 +0200

    arrays -slides og counting arrays been made

---
+++ arrays.js
@@ -0,0 +1,54 @@
+"use strict";
+
+/* const letters = ['a','b','c','d','e','f','h'];
+console.log(letters[4]);
+
+const newarr = letters;
+newarr[4] ='*';
+
+console.log(letters[4]); */
+
+/* 
+const people = ["Harry", "Ron", "Hermonie"];
+
+let result;
+
+result = people.push("Draco");
+
+console.log(result);
+console.log(people);
+
+result = people.pop();
+
+result = people.push("Neville");
+result = people.push("Luna");
+
+result = people.slice(0,3);
+
+result = people.splice(1,0,"Cho");
+
+
+people[1] = "Ginny";
+
+result = people.push("Fred", "George");
+
+result = people.indexOf("Fred");
+
+result = people.splice(result,1); */
+
+
+const str = "abcdefghijklmn";
+const arr1 = str.split();
+
+const arr2 = Array.from(str);
+
+console.log(arr1);
+console.log(arr2);
+
+
+
+
+
+
+
+
