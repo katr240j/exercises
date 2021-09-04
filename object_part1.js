3b334935672c782c81853bcec77e55d2e87ca322
Katrín María <katr240j@stud.kea.dk>
2021-09-04 17:09:07 +0200

    slide 1-10

--- object_part1.js
+++ object_part1.js
@@ -1,1 +1,43 @@
 "use strict";
+
+
+// slide 1-9
+/* let person1 = { 
+
+    firstName : "Peter", 
+    
+    age : 29, 
+    
+    student: false 
+    
+    }; 
+    
+    console.log( person1.lastName ); 
+    person1.lastName = "Lind"; 
+    person1.lastName = undefined;
+    console.log(person1);
+    console.log( person1.lastName ); 
+    console.log( person1.middleName ); 
+    delete person1.lastName;
+    console.log( person1.lastName ); 
+
+ */
+
+    //slide 9 - 
+
+    const person1 = { 
+        firstName : "Peter", 
+        age : 29, 
+        student: false 
+        }; 
+
+        person1.age++;
+        console.log(person1);
+
+        const person2 = {
+            firstName: "katrín María",
+            age: 21,
+            student:true
+        };
+
+        person1 = person2;
