8317c3ed55cd17843cd60a645f5a2d0d34a4a994
Katrín María <katr240j@stud.kea.dk>
2021-09-06 15:43:34 +0200

    object comparing

---
+++ comparing_obejcts.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const student1 = {
+    firstName: "Harry",
+    lastName: "Potter"
+};
+
+const student2 = {
+    firstName: "Harry",
+    lastName: "Potter"
+};
+
+student1.firstName = student2.firstName;
+student1.lastName = student2.lastName;
+if( student1 == student2)   {
+    console.log("They are the same!");
+} else  {
+    console.log("They are not...");
+}
+//student1.firstName ="Potty";
+
+console.log(student1);
+console.log(student2);
--- objects_part2.js
+++ objects_part2.js
@@ -1,1 +1,22 @@
 "use strict";
+
+let person1 = { 
+    firstName : "Peter", 
+    age : 29, 
+    student: false 
+    }; 
+
+    let person2 = {
+        firstName: "other Peter",
+        age:29,
+        student:false
+    };
+    let person3 = person1;
+
+    person3.firstName ="changed";
+
+    person3 = person2;
+
+    person2.firstName ="Also changed";
+
+    person1 =person3;
